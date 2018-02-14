79b5f8d0b74ea23a7cb502c5b40f910f2483edbb
ljedesigns <landon@ljedesign.com>
2018-02-13 20:57:18 -0600

    Assignment 5

--- app/src/main/java/css/cis3334/heartratetracker/HeartRateAdapter.java
+++ app/src/main/java/css/cis3334/heartratetracker/HeartRateAdapter.java
@@ -2,7 +2,9 @@ package css.cis3334.heartratetracker;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,8 +33,30 @@ public class HeartRateAdapter extends ArrayAdapter<HeartRate> {
         View view = inflater.inflate(R.layout.heart_rate_row, null);
         HeartRate hr = hrList.getHeartRate(position);
 
-        TextView PulseTextView = (TextView) view.findViewById(R.id.textViewPulse);
-        PulseTextView.setText(hr.getPulse().toString());
+        TextView PulseTextView = view.findViewById(R.id.textViewPulse);
+        String PulseString = hr.getPulse().toString() + " - " + hr.getRangeName() + " - " + hr.getRangeDescrtiption() ;
+        int heartRateRange = hr.getRange();
+        int colorValue;
+        if(heartRateRange == 0) {
+            colorValue = R.color.colorZone1;
+        }
+        else if(heartRateRange == 1) {
+            colorValue = R.color.colorZone2;
+        }
+        else if(heartRateRange == 2) {
+            colorValue = R.color.colorZone3;
+        }
+        else if(heartRateRange == 3) {
+            colorValue = R.color.colorZone4;
+        }
+        else {
+            colorValue = R.color.colorZone5;
+        }
+        Spannable spannable = new SpannableString(PulseString);
+
+        spannable.setSpan(new ForegroundColorSpan(context.getResources().getColor(colorValue)), 0, hr.getPulse().toString().length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        PulseTextView.setText(spannable, TextView.BufferType.SPANNABLE);
+        //PulseTextView.setFontFeatureSettings();
 
         return(view);
 
